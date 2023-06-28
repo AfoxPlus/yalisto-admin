132f014655eb9443735d58d9bd0e8f74424fd84b
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-27 22:25:52 -0500

    Update router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,17 +4,19 @@ import { Login } from '../pages';
 import { ChildDashboardRoutes } from './ChildDashboardRoutes';
 import { DashBoardRoutes } from './DashBoardRoutes';
 
-const router = createBrowserRouter([
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/",
-        element: <DashBoardRoutes/>,
-        children: ChildDashboardRoutes
-    }
-])
+const routes = [
+  {
+      path: "/login",
+      element: <Login/>
+  },
+  {
+      path: "/",
+      element: <DashBoardRoutes/>,
+      children: ChildDashboardRoutes
+  }
+]
+
+const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? '/' : '/web-admin-yalisto/' })
 
 
 export const AppRouter = () => {
