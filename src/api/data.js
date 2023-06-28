2ee945865b35c0f8f0c83fcec4284b91cc24f626
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-27 22:17:31 -0500

    update

--- src/api/data.js
+++ src/api/data.js
@@ -58,5 +58,15 @@ export const productos = [
         precio: 15.20,
         cantidad: 5,
         enAplicativo: true
+    },
+    {
+        id: '#AHGA72',
+        tipo: 'Menu',
+        nombre: 'Cabrito norteño',
+        descripcion: 'Acompañado con su ensalada y refresco',
+        imagen_URL: 'https://s3.amazon/imagen.jpg',
+        precio: 15.20,
+        cantidad: 5,
+        enAplicativo: true
     }
 ]
